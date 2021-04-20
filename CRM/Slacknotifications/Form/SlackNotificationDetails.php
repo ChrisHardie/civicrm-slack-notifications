5312ec671d6f21ad7eb559022acdcee48132b0fa
Chris Hardie <chris@chrishardie.com>
2021-04-20 10:36:36 -0400

    Reformat form controller and add initial field setup

--- CRM/Slacknotifications/Form/SlackNotificationDetails.php
+++ CRM/Slacknotifications/Form/SlackNotificationDetails.php
@@ -7,72 +7,79 @@ use CRM_Slacknotifications_ExtensionUtil as E;
  *
  * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
  */
-class CRM_Slacknotifications_Form_SlackNotificationDetails extends CRM_Core_Form {
-  public function buildQuickForm() {
+class CRM_Slacknotifications_Form_SlackNotificationDetails extends CRM_CivirulesActions_Form_Form {
+	public function buildQuickForm() {
 
-    // add form elements
-    $this->add(
-      'select', // field type
-      'favorite_color', // field name
-      'Favorite Color', // field label
-      $this->getColorOptions(), // list of options
-      TRUE // is required
-    );
-    $this->addButtons(array(
-      array(
-        'type' => 'submit',
-        'name' => E::ts('Submit'),
-        'isDefault' => TRUE,
-      ),
-    ));
+		// Add form elements
 
-    // export form elements
-    $this->assign('elementNames', $this->getRenderableElementNames());
-    parent::buildQuickForm();
-  }
+		// Slack message template
+		$this->add(
+			'text', // field type
+			'message_template', // field name
+			'Slack message template', // field label
+			array( 'size' => 50 ),
+			true // is required
+		);
 
-  public function postProcess() {
-    $values = $this->exportValues();
-    $options = $this->getColorOptions();
-    CRM_Core_Session::setStatus(E::ts('You picked color "%1"', array(
-      1 => $options[$values['favorite_color']],
-    )));
-    parent::postProcess();
-  }
 
-  public function getColorOptions() {
-    $options = array(
-      '' => E::ts('- select -'),
-      '#f00' => E::ts('Red'),
-      '#0f0' => E::ts('Green'),
-      '#00f' => E::ts('Blue'),
-      '#f0f' => E::ts('Purple'),
-    );
-    foreach (array('1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e') as $f) {
-      $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', array(1 => $f));
-    }
-    return $options;
-  }
+		// Slack Channel
+		$this->add(
+			'text', // field type
+			'channel', // field name
+			'Slack Channel (blank for default associated with incoming webhook)', // field label
+			null,
+			false // is required
+		);
+		$this->addButtons(
+			array(
+				array(
+					'type'      => 'submit',
+					'name'      => E::ts( 'Submit' ),
+					'isDefault' => false,
+				),
+			)
+		);
 
-  /**
-   * Get the fields/elements defined in this form.
-   *
-   * @return array (string)
-   */
-  public function getRenderableElementNames() {
-    // The _elements list includes some items which should not be
-    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-    // items don't have labels.  We'll identify renderable by filtering on
-    // the 'label'.
-    $elementNames = array();
-    foreach ($this->_elements as $element) {
-      /** @var HTML_QuickForm_Element $element */
-      $label = $element->getLabel();
-      if (!empty($label)) {
-        $elementNames[] = $element->getName();
-      }
-    }
-    return $elementNames;
-  }
+		// export form elements
+		$this->assign( 'elementNames', $this->getRenderableElementNames() );
+		parent::buildQuickForm();
+	}
 
+	public function setDefaultValues() {
+		$defaultValues = parent::setDefaultValues();
+		$data          = unserialize( $this->ruleAction->action_params, array( 'allowed_classes' => true ) );
+
+		if ( ! empty( $data['channel'] ) ) {
+			$defaultValues['channel'] = $data['channel'];
+		}
+		if ( ! empty( $data['message_template'] ) ) {
+			$defaultValues['message_template'] = $data['message_template'];
+		} else {
+			$defaultValues['message_template'] = 'The rule was triggered.';
+ 		}
+
+		return $defaultValues;
+	}
+
+	public function postProcess() {
+		parent::postProcess();
+	}
+
+	/**
+	 * Get the fields/elements defined in this form.
+	 *
+	 * @return array (string)
+	 */
+	public function getRenderableElementNames() {
+		$elementNames = array();
+		foreach ( $this->_elements as $element ) {
+			/** @var HTML_QuickForm_Element $element */
+			$label = $element->getLabel();
+			if ( ! empty( $label ) ) {
+				$elementNames[] = $element->getName();
+			}
+		}
+
+		return $elementNames;
+	}
 }
