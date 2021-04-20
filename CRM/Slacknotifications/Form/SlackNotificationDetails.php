7e3e97064f0521560d8d4a69bd49506900b217bd
Chris Hardie <chris@chrishardie.com>
2021-04-20 15:56:01 -0400

    More uncomfortable code standards work

--- CRM/Slacknotifications/Form/SlackNotificationDetails.php
+++ CRM/Slacknotifications/Form/SlackNotificationDetails.php
@@ -1,101 +1,123 @@
 <?php
 
-use CRM_Slacknotifications_ExtensionUtil as E;
-
 /**
- * Form controller class
+ * Form controller class.
  *
  * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
  */
 class CRM_Slacknotifications_Form_SlackNotificationDetails extends CRM_CivirulesActions_Form_Form {
-	public function buildQuickForm() {
 
-		// Add form elements
-		$this->setFormTitle();
-		$this->add( 'hidden', 'rule_action_id' );
+  /**
+   * Build the form.
+   */
+  public function buildQuickForm() {
 
-		// Slack message template
-		$this->add(
-			'text', // field type
-			'message_template', // field name
-			'Slack message template', // field label
-			array( 'size' => 50 ),
-			true // is required
-		);
+    // Add form elements.
+    $this->setFormTitle();
+    $this->add('hidden', 'rule_action_id');
 
+    // Slack message template.
+    $this->add(
+    // Field type.
+    'text',
+    // Field name.
+    'message_template',
+    // Field label.
+    'Slack message template',
+    ['size' => 50],
+    // Is required.
+    TRUE
+     );
 
-		// Slack Channel
-		$this->add(
-			'text', // field type
-			'channel', // field name
-			'Slack Channel<br />(Blank for default associated with incoming webhook.)', // field label
-			array( 'placeholder' => '#channel' ),
-			false // is required
-		);
-		$this->addButtons(
-			array(
-				array(
-					'type'      => 'next',
-					'name'      => ts( 'Save' ),
-					'isDefault' => true,
-				),
-				array(
-					'type' => 'cancel',
-					'name' => ts( 'Cancel' ),
-				),
-			)
-		);
+    // Slack Channel.
+    $this->add(
+    // Field type.
+    'text',
+    // Field name.
+    'channel',
+    // Field label.
+    'Slack Channel<br />(Blank for default associated with incoming webhook.)',
+    ['placeholder' => '#channel'],
+    // Is required.
+    FALSE
+     );
+    $this->addButtons(
+    [
+                [
+                  'type'      => 'next',
+                  'name'      => ts('Save'),
+                  'isDefault' => TRUE,
+                ],
+                [
+                  'type' => 'cancel',
+                  'name' => ts('Cancel'),
+                ],
+    ]
+     );
 
-		// export form elements
-		$this->assign( 'elementNames', $this->getRenderableElementNames() );
-		parent::buildQuickForm();
-	}
+    // Export form elements.
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
 
-	public function setDefaultValues() {
-		$defaultValues = parent::setDefaultValues();
-		$data          = unserialize( $this->ruleAction->action_params, array( 'allowed_classes' => true ) );
+  /**
+   * Set the default values.
+   */
+  public function setDefaultValues(): array {
+    $defaultValues = parent::setDefaultValues();
+    $data = unserialize($this->ruleAction->action_params, ['allowed_classes' => TRUE]);
 
-		if ( ! empty( $data['channel'] ) ) {
-			$defaultValues['channel'] = $data['channel'];
-		}
-		if ( ! empty( $data['message_template'] ) ) {
-			$defaultValues['message_template'] = $data['message_template'];
-		} else {
-			$defaultValues['message_template'] = 'The rule was triggered.';
- 		}
+    if (!empty($data['channel'])) {
+      $defaultValues['channel'] = $data['channel'];
+    }
+    if (!empty($data['message_template'])) {
+      $defaultValues['message_template'] = $data['message_template'];
+    }
+    else {
+      $defaultValues['message_template'] = 'The rule was triggered.';
+    }
 
-		return $defaultValues;
-	}
+    return $defaultValues;
+  }
 
-	protected function getSubmittedData() {
-		$data = array();
-		$data['message_template'] = $this->_submitValues['message_template'];
-		$data['channel'] = $this->_submitValues['channel'];
-		return $data;
-	}
+  /**
+   * Get the submitted data.
+   */
+  protected function getSubmittedData(): array {
+    $data = [];
+    $data['message_template'] = $this->_submitValues['message_template'];
+    $data['channel'] = $this->_submitValues['channel'];
 
-	public function postProcess() {
-		$data = $this->getSubmittedData();
-		$this->ruleAction->action_params = serialize($data);
-		$this->ruleAction->save();
-		parent::postProcess();
-	}
+    return $data;
+  }
 
-	/**
-	 * Get the fields/elements defined in this form.
-	 *
-	 * @return array (string)
-	 */
-	public function getRenderableElementNames() {
-		$elementNames = array();
-		foreach ( $this->_elements as $element ) {
-			/** @var HTML_QuickForm_Element $element */
-			$label = $element->getLabel();
-			if ( ! empty( $label ) ) {
-				$elementNames[] = $element->getName();
-			}
-		}
+  /**
+   * Process the submitted data.
+   */
+  public function postProcess() {
+    $data = $this->getSubmittedData();
+    $this->ruleAction->action_params = serialize($data);
+    $this->ruleAction->save();
+    parent::postProcess();
+  }
 
-		return $elementNames;
-	}
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array
+   *   (string)
+   */
+  public function getRenderableElementNames(): array {
+    $elementNames = [];
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+
+    return $elementNames;
+  }
+
 }
